8bc6054f4cd92ffa1552ff78c0adb4a9b83c3014
Arc-ad <barkovskiia@bk.ru>
2022-06-07 23:43:37 +0300

    пароход

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import com.transport.*;
 import com.transport.publicTransport.Bus;
+import com.transport.steamTechnology.Steamer;
 
 public class Main {
     public static void main(String[] args) {
     Bus bus = new Bus();
     bus.setName("Школьный автобус");
         System.out.println(bus.getName());
+        Steamer steamer = new Steamer();
+        System.out.println(steamer.weight);
     }
 }
---
+++ src/com/transport/steamTechnology/Steamer.java
@@ -0,0 +1,7 @@
+package com.transport.steamTechnology;
+
+import com.transport.Transport;
+
+public class Steamer extends Transport {
+    public int weight = 200;
+}
